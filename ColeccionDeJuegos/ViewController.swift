47769f0cb5bb9134b329b022288e3334be1f12ad
j-e-m <jun.escobar.m@tecsup.edu.pe>
2023-05-17 15:13:54 -0500

    commit lab completo y con la tarea

--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -16,7 +16,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.isEditing = true
+
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                        _ = UITableViewCell()
+                let juego = juegos[indexPath.row]
+                let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                        context.delete(juego)
+                (UIApplication.shared.delegate as!  AppDelegate).saveContext()
+                juegos.remove(at: indexPath.row)
+                tableView.reloadData()
+            }
+        }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+            let movedObject = juegos[sourceIndexPath.row]
+                juegos.remove(at: sourceIndexPath.row)
+                juegos.insert(movedObject, at:destinationIndexPath.row)
+        }
+
+
+    
+    
+    
+    
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         juegos.count
@@ -30,6 +69,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
+    }
     
     
     override func viewWillAppear(_ animated: Bool) {
